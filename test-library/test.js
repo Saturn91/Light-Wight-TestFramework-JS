10c15f6692622e3cb42395a805f5cc61ffa4ea23
Manuel G <manuel.geissberger@gmail.com>
2021-10-07 08:47:38 +0200

    [EDIT] Update

--- test-library/test.js
+++ test-library/test.js
@@ -76,9 +76,8 @@ function assert(testDescription, test, expection) {
         TEST_SUITE.runs_scope.failedTests++;
         console.error(testDescription + ' -> failed')
     }  
-    if(!passed || TEST_SUITE.runAllTestsComplete) {
-        testResultLine(testDescription, passed);
-    }   
+    if(!passed || TEST_SUITE.runAllTestsComplete) testResultLine(testDescription, passed);
+       
     TEST_SUITE.lastTitle.element.innerText = TEST_SUITE.lastTitle.text +  `: ${TEST_SUITE.runs_scope.testRuns} tests ran, ${TEST_SUITE.runs_scope.passedTests}/${TEST_SUITE.runs_scope.testRuns} passed, ${TEST_SUITE.runs_scope.failedTests} failed` 
     TEST_SUITE.summaryLine.innerText =                                      `${TEST_SUITE.runs_total.testRuns} tests ran, ${TEST_SUITE.runs_total.passedTests}/${TEST_SUITE.runs_total.testRuns} passed, ${TEST_SUITE.runs_total.failedTests} failed`
 }
